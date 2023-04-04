4ada7d4264b4ace1ae4cba5b59662670ad0df630
Jake Robinson <mr_jaker@hotmail.com>
2023-04-04 22:25:18 +1000

    color options

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,10 @@
                 if (ifExistsRemove) {
                     ifExistsRemove.parentNode.removeChild(ifExistsRemove);
                     }
+                    ifExistsRemove = document.querySelector('.top10Boxes');
+                            if (ifExistsRemove) {
+                                ifExistsRemove.parentNode.removeChild(ifExistsRemove);
+                                }
 
         // wrap this all into our own minimal object containing only the information we need
         let top10Info = 
